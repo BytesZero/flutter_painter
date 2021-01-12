6d6e1e096214dc659c2c3c991b76cc756cfb1474
Zero <1300326388@qq.com>
2021-01-12 16:30:55 +0800

    1、添加文字删除功能

--- example/lib/draw/draw_text.dart
+++ example/lib/draw/draw_text.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/gestures.dart';
-
 import 'base_draw.dart';
 
 /// 绘制文字
@@ -7,20 +5,83 @@ class DrawText extends BaseDraw {
   String text; // 文字
   Color color = Color(0xFFFFFFFF); // 颜色
   double fontSize = 14; // 文字大小
+  double scale = 1.0; // 缩放
+  TextPainter tp; // 文字画笔
+  // 文字矩阵
+  Rect get textRect => Rect.fromLTWH(
+        offset.dx - 4,
+        offset.dy - 4,
+        tp.width + 8,
+        tp.height + 8,
+      );
+
+  // 是否选中
+  bool selected = false;
+  // 删除半径
+  double delRadius = 6;
 
   @override
   void draw(Canvas canvas, Size size) {
     if (text?.isEmpty ?? true) {
       return;
     }
+    canvas.save();
+    // 缩放
+    canvas.scale(scale);
     // 设置央视
     TextStyle style = TextStyle(fontSize: fontSize, color: color);
     // 设置问绷
-    TextSpan textSpan = TextSpan(text: text, style: style,recognizer: ForcePressGestureRecognizer());
+    TextSpan textSpan = TextSpan(
+      text: text,
+      style: style,
+    );
     // 设置文本画笔
-    TextPainter tp =
-        TextPainter(text: textSpan, textDirection: TextDirection.ltr);
-    tp.layout(minWidth: 20.0, maxWidth: size.width);
+    tp = TextPainter(
+      text: textSpan,
+      textAlign: TextAlign.left,
+      textDirection: TextDirection.ltr,
+    );
+    // 布局文字
+    tp.layout(minWidth: drawSize.width, maxWidth: size.width - offset.dx - 4);
+    // 绘制文字
     tp.paint(canvas, offset);
+    if (selected) {
+      /// 设置边框画笔
+      paint
+        ..color = Color(0xFFFFFFFF)
+        ..isAntiAlias = true
+        ..strokeWidth = 1
+        ..style = PaintingStyle.stroke;
+
+      /// 绘制边框
+      canvas.drawRect(
+        textRect,
+        paint,
+      );
+      canvas.save();
+
+      /// 设置删除画笔
+      paint.style = PaintingStyle.fill;
+      //画圆形背景
+      canvas.drawCircle(Offset(textRect.left, textRect.top), delRadius, paint);
+      // 设置 X 画笔
+      paint
+        ..style = PaintingStyle.stroke
+        ..color = Color(0xFF999999);
+      double xRadius = delRadius / 3.0;
+      // 画 X
+      canvas.drawLine(
+        Offset(textRect.left - xRadius, textRect.top - xRadius),
+        Offset(textRect.left + xRadius, textRect.top + xRadius),
+        paint,
+      );
+      canvas.drawLine(
+        Offset(textRect.left + xRadius, textRect.top - xRadius),
+        Offset(textRect.left - xRadius, textRect.top + xRadius),
+        paint,
+      );
+      canvas.restore();
+    }
+    canvas.restore();
   }
 }
--- example/lib/home_page.dart
+++ example/lib/home_page.dart
@@ -51,10 +51,17 @@ class _HomePageState extends State<HomePage>
   Color selectColor = Colors.red;
   // 绘制集合
   List<BaseDraw> paintList = [];
+  // 临时线
   DrawLine _tempLine;
+  // 临时文字，标记选中赋值
+  DrawText _tempText;
+  // 临时按下事件记录，防止时间错乱
+  TapDownDetails _tempTapDownDetails;
 
   @override
   bool get wantKeepAlive => true;
+
+  Offset textOffset = Offset(100, 40);
 
   @override
   Widget build(BuildContext context) {
@@ -75,66 +82,136 @@ class _HomePageState extends State<HomePage>
             child: Stack(
               children: [
                 Image.asset(imageUrl),
-                boradMode == BoradMode.Draw
-                    ? GestureDetector(
-                        key: drawGestureKey,
-                        onPanStart: (details) {
-                          _tempLine = DrawLine();
-                          _tempLine.color = selectColor;
-                          paintList.add(_tempLine);
-                        },
-                        onPanUpdate: (details) {
-                          RenderBox renderBox =
-                              drawGestureKey.currentContext.findRenderObject();
-                          Offset localPos =
-                              renderBox.globalToLocal(details.globalPosition);
-                          if (_tempLine == null) {
-                            _tempLine = DrawLine();
-                            paintList.add(_tempLine);
-                          }
-                          _tempLine.linePath.add(localPos);
-                          paintList.last = _tempLine;
-                          setState(() {});
-                        },
-                        onPanEnd: (details) {
-                          _tempLine = null;
-                        },
-                      )
-                    : SizedBox(),
-                boradMode == BoradMode.Zoom
-                    ? GestureDetector(
-                        onScaleStart: (details) {
-                          _tmpFocal = details.focalPoint;
-                          _tmpMoveX = _moveX;
-                          _tmpMoveY = _moveY;
-                          _tmpScale = _scale;
-                          _tmpRotation = _rotation;
-
-                          debugPrint(
-                              'onScaleStart _tmpFocal:$_tmpFocal _tmpMoveX:$_tmpMoveX _tmpMoveY:$_tmpMoveY _tmpScale:$_tmpScale _tmpRotation:$_tmpRotation details:${details.toString()}');
-                        },
-                        onScaleUpdate: (details) {
-                          debugPrint(
-                              'onScaleUpdate details:${details.toString()}');
-                          _moveX = _tmpMoveX +
-                              (details.focalPoint.dx - _tmpFocal.dx) /
-                                  _tmpScale;
-                          debugPrint(
-                              'onScaleUpdate _moveX:$_moveX _tmpMoveX:$_tmpMoveX _tmpFocal:${_tmpFocal.toString()} _tmpScale:$_tmpScale');
-                          _moveY = _tmpMoveY +
-                              (details.focalPoint.dy - _tmpFocal.dy) /
-                                  _tmpScale;
-                          debugPrint(
-                              'onScaleUpdate _moveY:$_moveY _tmpMoveY:$_tmpMoveY _tmpFocal:${_tmpFocal.toString()} _tmpScale:$_tmpScale');
-                          _scale = _tmpScale * details.scale;
-                          _rotation = _tmpRotation + details.rotation;
-                          setState(() {});
-                        },
-                      )
-                    : SizedBox(),
                 CustomPaint(
-                  size: MediaQuery.of(context).size,
+                  size: Size.infinite,
                   painter: DrawBorad(paintList: paintList),
+                  child: boradMode == BoradMode.Zoom
+                      ? GestureDetector(
+                          onTapDown: (details) {
+                            // 设置按下事件信息
+                            _tempTapDownDetails = details;
+                          },
+                          onTap: () {
+                            debugPrint('onTap');
+                            Offset lp = _tempTapDownDetails.localPosition;
+                            debugPrint('onTapDown details:${lp.toString()}');
+                            debugPrint(
+                                'onTapDown _tempText:${_tempText.toString()}');
+                            if (_tempText != null) {
+                              /// 计算是否命中删除区域
+                              double delRadius = _tempText.delRadius;
+                              Rect tempTextRect = _tempText.textRect;
+                              // tempTextRect = tempTextRect.deflate(
+                              //     (tempTextRect.right - tempTextRect.left) *
+                              //         _tempText.scale);
+                              if (_tempText.selected &&
+                                  lp.dx >= (tempTextRect.left - delRadius) &&
+                                  lp.dx <= (tempTextRect.left + delRadius) &&
+                                  lp.dy >= (tempTextRect.top - delRadius) &&
+                                  lp.dy <= (tempTextRect.top + delRadius)) {
+                                paintList.remove(_tempText);
+                                _tempText = null;
+                                setState(() {});
+                                return;
+                              } else {
+                                _tempText.selected = false;
+                              }
+                            }
+
+                            /// 只获取文字
+                            var textList = paintList.whereType<DrawText>();
+                            // 遍历查看是否命中事件
+                            for (var item in textList) {
+                              Rect textRect = item.textRect;
+                              //计算是否命中事件
+                              if (lp.dx >= textRect.left &&
+                                  lp.dx <= textRect.right &&
+                                  lp.dy >= textRect.top &&
+                                  lp.dy <= textRect.bottom) {
+                                debugPrint('onTapDown 命中🎯');
+                                _tempText = item;
+                                _tempText.selected = true;
+                                setState(() {});
+                                break;
+                              } else {
+                                debugPrint('onTapDown 未命中');
+                                _tempText = null;
+                                setState(() {});
+                              }
+                            }
+                          },
+                          onScaleStart: (details) {
+                            _tmpFocal = details.focalPoint;
+                            if (_tempText != null && _tempText.selected) {
+                              _tmpMoveX = _tempText.offset.dx;
+                              _tmpMoveY = _tempText.offset.dy;
+                              _tmpScale = _tempText.scale;
+                            } else {
+                              _tmpMoveX = _moveX;
+                              _tmpMoveY = _moveY;
+                              _tmpScale = _scale;
+                              _tmpRotation = _rotation;
+                            }
+
+                            debugPrint(
+                                'onScaleStart _tmpFocal:$_tmpFocal _tmpMoveX:$_tmpMoveX _tmpMoveY:$_tmpMoveY _tmpScale:$_tmpScale _tmpRotation:$_tmpRotation details:${details.toString()}');
+                          },
+                          onScaleUpdate: (details) {
+                            if (_tempText != null && _tempText.selected) {
+                              double textMoveX = _tmpMoveX +
+                                  (details.focalPoint.dx - _tmpFocal.dx) /
+                                      _tmpScale;
+                              debugPrint(
+                                  'onScaleUpdate _moveX:$_moveX _tmpMoveX:$_tmpMoveX _tmpFocal:${_tmpFocal.toString()} _tmpScale:$_tmpScale');
+                              double textMoveY = _tmpMoveY +
+                                  (details.focalPoint.dy - _tmpFocal.dy) /
+                                      _tmpScale;
+                              _tempText.offset = Offset(textMoveX, textMoveY);
+                              // _tempText.scale = _tmpScale * details.scale;
+                            } else {
+                              debugPrint(
+                                  'onScaleUpdate details:${details.toString()}');
+                              _moveX = _tmpMoveX +
+                                  (details.focalPoint.dx - _tmpFocal.dx) /
+                                      _tmpScale;
+                              debugPrint(
+                                  'onScaleUpdate _moveX:$_moveX _tmpMoveX:$_tmpMoveX _tmpFocal:${_tmpFocal.toString()} _tmpScale:$_tmpScale');
+                              _moveY = _tmpMoveY +
+                                  (details.focalPoint.dy - _tmpFocal.dy) /
+                                      _tmpScale;
+                              debugPrint(
+                                  'onScaleUpdate _moveY:$_moveY _tmpMoveY:$_tmpMoveY _tmpFocal:${_tmpFocal.toString()} _tmpScale:$_tmpScale');
+                              _scale = _tmpScale * details.scale;
+                              _rotation = _tmpRotation + details.rotation;
+                            }
+
+                            setState(() {});
+                          },
+                        )
+                      : GestureDetector(
+                          key: drawGestureKey,
+                          onPanStart: (details) {
+                            _tempLine = DrawLine();
+                            _tempLine.color = selectColor;
+                            paintList.add(_tempLine);
+                          },
+                          onPanUpdate: (details) {
+                            RenderBox renderBox = drawGestureKey.currentContext
+                                .findRenderObject();
+                            Offset localPos =
+                                renderBox.globalToLocal(details.globalPosition);
+                            if (_tempLine == null) {
+                              _tempLine = DrawLine();
+                              paintList.add(_tempLine);
+                            }
+                            _tempLine.linePath.add(localPos);
+                            paintList.last = _tempLine;
+                            setState(() {});
+                          },
+                          onPanEnd: (details) {
+                            _tempLine = null;
+                          },
+                        ),
                 ),
               ],
             ),
@@ -146,40 +223,39 @@ class _HomePageState extends State<HomePage>
         mainAxisAlignment: MainAxisAlignment.center,
         mainAxisSize: MainAxisSize.min,
         children: [
-          Visibility(
-            visible: true,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.black.withOpacity(0.1),
-                borderRadius: BorderRadius.circular(20),
-              ),
-              padding: EdgeInsets.all(10),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: colorList.map((color) {
-                  return GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        selectColor = color;
-                      });
-                    },
-                    child: Container(
-                      height: 24,
-                      width: 24,
-                      margin: EdgeInsets.all(6),
-                      padding: EdgeInsets.all(4),
-                      decoration: BoxDecoration(
-                        color: color,
-                        borderRadius: BorderRadius.circular(12),
-                        border: Border.all(
-                          color: Colors.white,
-                          width: selectColor == color ? 4 : 2,
-                        ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.black.withOpacity(0.1),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            padding: EdgeInsets.all(10),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: colorList.map((color) {
+                return GestureDetector(
+                  onTap: () {
+                    selectColor = color;
+                    if (_tempText != null && _tempText.selected) {
+                      _tempText.color = selectColor;
+                    }
+                    setState(() {});
+                  },
+                  child: Container(
+                    height: 24,
+                    width: 24,
+                    margin: EdgeInsets.all(6),
+                    padding: EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                      color: color,
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(
+                        color: Colors.white,
+                        width: selectColor == color ? 4 : 2,
                       ),
                     ),
-                  );
-                }).toList(),
-              ),
+                  ),
+                );
+              }).toList(),
             ),
           ),
           SizedBox(height: 20),
@@ -244,10 +320,10 @@ class _HomePageState extends State<HomePage>
                 tooltip: '文本',
                 onPressed: () {
                   DrawText drawText = DrawText()
-                    ..text =
-                        '花熊使\n用低级方法时dart:ui，习惯上以前缀这些类ui.。\n这也有助于解决命名冲突。例如，TextStyle还可以在绘画库中定义。如果使用了TextStyle，则您需要为单个样式dart:ui使用编码，TextStyle().getTextStyle()或TextStyle().build()递归地应用样式树。'
-                    ..drawSize = Size(100, 100)
+                    ..text = '花熊是\n最可爱的狗狗🐶'
+                    ..drawSize = Size(0, 0)
                     ..offset = Offset(80, 80)
+                    ..fontSize = (12 + paintList.length.toDouble())
                     ..color = selectColor;
                   paintList.add(drawText);
                   setState(() {});
