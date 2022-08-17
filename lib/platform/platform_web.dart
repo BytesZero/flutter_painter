bb937c92aa6918313ada725d5b9f0b47f89cdccf
Zero <1300326388@qq.com>
2022-08-17 11:11:05 +0800

    1、更新说明和版本

--- lib/platform/platform_web.dart
+++ lib/platform/platform_web.dart
@@ -1,6 +1,14 @@
 import 'dart:html';
 
+/// 禁用右键菜单
 void disableRightClick() {
+  document.onContextMenu.listen((e) {
+    e.preventDefault();
+  });
+}
+
+/// 启用右键菜单
+void enableRightClick() {
   document.onContextMenu.listen((e) {
     e.preventDefault();
   });
